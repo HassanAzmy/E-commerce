8e78408a6e5baf7ced9975b2844bdf3aa553a045
Hassan Azmy <hassanazmy13579@gmail.com>
2025-01-26 17:20:40 +0200

    PArsing Incoming Requests

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+
+//* body-parser is a middleware that parses the incoming request body
+//* extended: false means that the body-parser will only parse simple bodies for URL encoded data
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-   console.log('In the second middleware!');
-   res.send('<h1>Hello from add product page!</h1>');
+   res.send(`
+      <form action="/product" method="POST">
+         <input type="text" name="title">
+         <button type="submit">Add Product</button>
+      </form>
+   `);
 });
 
-app.use('/payment', (req, res, next) => {
-   console.log('In the second middleware!');
-   res.send('<h1>Hello from payment page!</h1>');
+app.use('/product', (req, res, next) => {
+   //NOTE: without a body parser, we can't parse the incoming request body
+   console.log(req.body);  //* undefined
+   res.redirect('/');
 });
 
 //* default path for all routes
 app.use('/', (req, res, next) => {
-   console.log('In the second middleware!');
    res.send('<h1>Hello from Express!</h1>');
-});
-
-//* won't be reached because of the previous middleware
-app.use('/azmy', (req, res, next) => {
-   console.log('In the second middleware!');
-   res.send('<h1>Hello from azmy page!</h1>');
 });
 
 app.listen(3000);
