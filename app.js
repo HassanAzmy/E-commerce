6492d10989be7d24913a6b8622c93a7f1ee50b08
Hassan Azmy <hassanazmy13579@gmail.com>
2025-01-27 08:40:54 +0200

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(shopRouter);
 app.use(adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+   // res.statusCode = 404;
+   res.status(404).send('<h1>404 Page not found</h1>');
+});
 
 app.listen(3000);
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-//* using get instead of use checking the exact path
 router.get('/', (req, res, next) => {
    res.send('<h1>Hello from Express!</h1>');
 });
