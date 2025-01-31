7651e300d0151661dff723d238e794995345676c
Hassan Azmy <hassanazmy13579@gmail.com>
2025-01-31 16:03:13 +0200

    Converting our Project to Handlebars

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const handlebars = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', handlebars.engine(
+   {defaultLayout: false,}
+));
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,7 +25,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
    res.status(404).render('404', { pageTitle: 'Page Not Found', message: 'Page Not Found!'});
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const { log } = require('console');
 router.get('/', (req, res, next) => {
    const products = adminData.products;
    
-   res.render('shop', {prods: products, pageTitle: 'Shop'});
+   res.render('shop', {prods: products, pageTitle: 'Shop', hasProducts: products.length > 0});
 });
 
 module.exports = router;
