9b41912dae4756f85db988a283adab56858c86a4
Hassan Azmy <hassanazmy13579@gmail.com>
2025-01-31 21:02:44 +0200

    Converting our Project to EJS

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const handlebars = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const app = express();
 
-//* hbs is the file extension for handlebars files except the main layout file
-app.engine('hbs', handlebars.engine({
-      defaultLayout: 'main-layout',
-      layoutsDir: 'views/layouts/',
-      extname: 'hbs' //* this extension is applied only to the layout file
-   }
-));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
