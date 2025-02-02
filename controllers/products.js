98785adf88000e06117e937fc54548a5a95b187d
Hassan Azmy <hassanazmy13579@gmail.com>
2025-02-02 14:32:22 +0200

    Adding a Product Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,6 @@
-const products = [];
+// const products = [];
+
+const Product = require('../models/Product');
 
 exports.getAddProduct = (req, res, next) => {
    res.render('add-product', {
@@ -7,11 +9,15 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-   products.push({ title: req.body.title });
+   // products.push({ title: req.body.title });
+   const product = new Product(req.body.title);
+   // products.push(product);
+   product.save();
    res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+   const products = Product.fetchAll();
    res.render('shop', {
       prods: products,
       pageTitle: 'Shop'
