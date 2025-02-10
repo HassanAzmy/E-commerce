d9f6c729306b5939e30d27f80a1957f453966c6b
Hassan Azmy <hassanazmy13579@gmail.com>
2025-02-10 04:39:47 +0200

    Adding a One-To-Many Relationship (Handeling Association)

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const errorController = require('./controllers/errorController');
 const sequelize = require('./utility/database');
+const Product = require('./models/ProductModel');
+const User = require('./models/userModel');
 const { log } = require('console');
 
 const app = express();
@@ -25,8 +27,13 @@ app.use('/admin', adminRouter);
 app.use(shopRouter);
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 //* It syncs models to the database by creating tables
-sequelize.sync()
+//* It also defines the relations in the database
+//* force: true => to forcly override the database if exists
+sequelize.sync({force: true})
    .then(result => {
       console.log('Server has been started');
       app.listen(3000);
